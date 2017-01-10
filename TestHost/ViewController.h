2b9a0f232d7d53121f76b63f6fab91f0d9f37511
Vadim Dagman <vadim@digitalprunes.com>
2017-01-10 12:52:43 -0800

    Host app to be able to test on iOS device

---
+++ TestHost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestHost
+//
+//  Created by Vadim on 1/10/17.
+//  Copyright © 2017 Token Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestHost/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestHost
+//
+//  Created by Vadim on 1/10/17.
+//  Copyright © 2017 Token Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
