c33b77d27f92613c8b81c2e4d9d7abc4af0028a8
Vadim Dagman <vadim@digitalprunes.com>
2017-01-10 18:53:55 -0800

    Secure Enclave Engine

--- tests/TKSecureEnclaveCryptoEngine.m
+++ tests/TKSecureEnclaveCryptoEngine.m
@@ -7,6 +7,10 @@
 //
 
 #import "TKSecureEnclaveCryptoEngine.h"
+#import "TKKeyInfo.h"
+#import "TKJson.h"
+#import "TKSignature.h"
+#include <CommonCrypto/CommonDigest.h>
 
 @implementation TKSecureEnclaveCryptoEngine {
     NSString* _memberId;
@@ -20,33 +24,77 @@
     return self;
 }
 
-- (NSArray<TKKeyInfo*> *)generateKeys {
+- (TKKeyInfo*)generateKey:(Key_Level)level {
+    return [self generateKeyPairWithLevel:level];
+}
+
+- (TKSignature*)signData:(NSData *)data usingKeyLevel:(Key_Level)keyLevel {
+    SecKeyRef privateKeyRef = [self privateKeyForLevel:keyLevel];
+    CFErrorRef error = NULL;
+
+    CFDataRef signRef = SecKeyCreateSignature(privateKeyRef, kSecKeyAlgorithmECDSASignatureMessageX962SHA1, (__bridge CFDataRef)data, &error);
+    if (error != errSecSuccess) {
+        [NSException
+         raise:NSInvalidArgumentException
+         format:@"Error signing data: %@\n", error];
+    }
+    NSData* signatureData = (__bridge NSData *)(signRef);
+
+    NSString* signatureString = [TKUtil base64EncodeData:signatureData];
+    TKSignature* tkSignature =  [TKSignature signature:signatureString signedWith:[self keyInfoForPrivateKey:privateKeyRef level:keyLevel]];
+    CFRelease(privateKeyRef);
+
+    return tkSignature;
+}
+
+- (bool)verifySignature:(NSString *)signature forData:(NSData *)data usingKeyId:(NSString *)keyId {
+    SecKeyRef keyRef = [self publicKeyForKeyId:keyId];
+    if (keyRef == NULL) {
+        return false;
+    }
+    CFErrorRef error = NULL;
+    NSData* signatureData = [TKUtil base64DecodeString:signature];
+    Boolean success = SecKeyVerifySignature(keyRef, kSecKeyAlgorithmECDSASignatureMessageX962SHA1, (__bridge CFDataRef)data, (__bridge CFDataRef)(signatureData), &error);
+    CFRelease(keyRef);
     
+    return success == 1;
 }
 
 #pragma mark- Private Methods
 
-- (NSString*)keyLabelForKeyType:(TKKeyType)keyType {
-    return [NSString stringWithFormat:@"%@_%@", _memberId, @(keyType)];
+#define newCFDict CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks)
+
+
+- (NSString*)keyLabelForKeylevel:(Key_Level)level {
+    return [NSString stringWithFormat:@"%@_%@", _memberId, @(level)];
 }
 
-- (TKKeyInfo*)generateKeyPairOfType:(TKKeyType)type {
+- (TKKeyInfo*)generateKeyPairWithLevel:(Key_Level)level {
+    
+    [self deleteOldKeysForLevel:level];
+    
     CFErrorRef error = NULL;
+    SecAccessControlCreateFlags accessFlags = kSecAccessControlPrivateKeyUsage;
+    if (level < Key_Level_Low) {
+        accessFlags |= kSecAccessControlUserPresence; // Will require Touch ID/Passcode
+    }
     SecAccessControlRef sacObject = SecAccessControlCreateWithFlags(
                                                                     kCFAllocatorDefault,
                                                                     kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly,
-                                                                    kSecAccessControlUserPresence | kSecAccessControlPrivateKeyUsage,
+                                                                    accessFlags,
                                                                     &error
                                                                     );
     
     if (error != errSecSuccess) {
-        NSLog(@"Error generating access controls: %@\n", error);
-        return nil;
+        [NSException
+         raise:NSInvalidArgumentException
+         format:@"Error generating access controls: %@\n", error];
     }
     
-    CFMutableDictionaryRef accessControlDict = newCFDict;;
+    CFMutableDictionaryRef accessControlDict = newCFDict;
     CFDictionaryAddValue(accessControlDict, kSecAttrAccessControl, sacObject);
     CFDictionaryAddValue(accessControlDict, kSecAttrIsPermanent, kCFBooleanTrue);
+    CFDictionaryAddValue(accessControlDict, kSecAttrLabel, (__bridge const void *)([self keyLabelForKeylevel:level]));
     
     // create dict which actually saves key into keychain
     CFMutableDictionaryRef generateKeyRef = newCFDict;
@@ -55,20 +103,109 @@
     CFDictionaryAddValue(generateKeyRef, kSecAttrKeySizeInBits, (__bridge const void *)([NSNumber numberWithInt:256]));
     CFDictionaryAddValue(generateKeyRef, kSecPrivateKeyAttrs, accessControlDict);
     
+    CFRelease(sacObject);
+    
     SecKeyRef privateKeyRef = SecKeyCreateRandomKey(generateKeyRef, &error);
     if (error != errSecSuccess) {
-        NSLog(@"Error generating private key: %@\n", error);
-        return nil;
+        [NSException
+         raise:NSInvalidArgumentException
+         format:@"Error generating private key: %@\n", error];
     }
     
-    CFDictionaryRef dictRef = SecKeyCopyAttributes(privateKeyRef);
-    
-    CFDataRef keyHash = CFDictionaryGetValue(dictRef, kSecAttrApplicationLabel);
-    NSData* keyHashData = (__bridge NSData *)(keyHash);
-    NSString* keyHashString = [TKUtil base64EncodeData:keyHashData];
-    NSLog(@"Key Hash String %@", keyHashString);
-    
-    return keyHashString;
+    TKKeyInfo* keyInfo = [self keyInfoForPrivateKey:privateKeyRef level:level];
+
+    CFRelease(privateKeyRef);
+    return keyInfo;
 }
 
+- (void)deleteOldKeysForLevel:(Key_Level)level {
+    CFMutableDictionaryRef queryKeyRef = newCFDict;
+    CFDictionaryAddValue(queryKeyRef, kSecAttrTokenID, kSecAttrTokenIDSecureEnclave);
+    CFDictionaryAddValue(queryKeyRef, kSecAttrLabel, (__bridge const void *)([self keyLabelForKeylevel:level]));
+    CFDictionarySetValue(queryKeyRef, kSecClass, kSecClassKey);
+    SecItemDelete(queryKeyRef);
+}
+
+- (NSData*)publicKeyDataFromKeyRef:(SecKeyRef)publicKeyRef {
+    CFMutableDictionaryRef queryRef = newCFDict;
+    CFDictionarySetValue(queryRef, kSecClass, kSecClassKey);
+    CFDictionarySetValue(queryRef, kSecValueRef, publicKeyRef);
+    CFDictionarySetValue(queryRef, kSecAttrKeyClass, kSecAttrKeyClassPublic);
+    CFDictionarySetValue(queryRef, kSecReturnData, kCFBooleanTrue);
+    CFTypeRef result;
+    OSStatus status = SecItemAdd(queryRef, (CFTypeRef *)&result);
+    if (status != errSecSuccess) {
+        status = SecItemCopyMatching(queryRef, (CFTypeRef *)&result);
+        if (status != errSecSuccess) {
+            [NSException
+             raise:NSInvalidArgumentException
+             format:@"Error adding public key: %@\n", @(status)];
+        }
+    }
+    NSData* keyBitsData = (__bridge NSData *)(result);
+    return keyBitsData;
+}
+
+- (TKKeyInfo*)keyInfoForPrivateKey:(SecKeyRef)keyRef level:(Key_Level)level {
+    SecKeyRef publicKeyRef = SecKeyCopyPublicKey(keyRef);
+    NSData* puclicKeyData = [self publicKeyDataFromKeyRef:publicKeyRef];
+    
+    CFDictionaryRef dictRef = SecKeyCopyAttributes(keyRef);
+    CFDataRef keyHash = CFDictionaryGetValue(dictRef, kSecAttrApplicationLabel);
+    NSLog(@"kSecAttrApplicationLabel %@", keyHash);
+    NSLog(@"puclicKeyData %@", puclicKeyData);
+    NSData* keyHashData = (__bridge NSData *)(keyHash);
+    NSString* keyHashString = [TKUtil base64EncodeData:keyHashData];
+    
+    CFRelease(publicKeyRef);
+   
+    return  [TKKeyInfo keyInfoWithId:keyHashString level:level algorithm:Key_Algorithm_Unknown publicKey:puclicKeyData];
+}
+
+- (SecKeyRef)privateKeyForLevel:(Key_Level)level {
+
+    CFMutableDictionaryRef getKeyRef = newCFDict;
+    CFDictionarySetValue(getKeyRef, kSecClass, kSecClassKey);
+    CFDictionarySetValue(getKeyRef, kSecAttrKeyClass, kSecAttrKeyClassPrivate);
+    CFDictionaryAddValue(getKeyRef, kSecAttrTokenID, kSecAttrTokenIDSecureEnclave);
+    CFDictionarySetValue(getKeyRef, kSecAttrLabel, (__bridge const void *)([self keyLabelForKeylevel:level]));
+    CFDictionarySetValue(getKeyRef, kSecReturnRef, kCFBooleanTrue);
+    CFDictionarySetValue(getKeyRef, kSecUseOperationPrompt, @"Authenticate to sign data");
+    
+    SecKeyRef keyRef;
+    OSStatus status = SecItemCopyMatching(getKeyRef, (CFTypeRef *)&keyRef);
+    if (status != errSecSuccess) {
+        [NSException
+         raise:NSInvalidArgumentException
+         format:@"Error retrieving private key: %@\n", @(status)];
+    }
+    
+    return (SecKeyRef)keyRef;
+}
+
+- (NSData *)createSHA512:(NSData *)source {
+    uint8_t digest[CC_SHA512_DIGEST_LENGTH] = {0};
+    CC_SHA512(source.bytes, (CC_LONG)source.length, digest);
+    return [NSData dataWithBytes:digest length:CC_SHA512_DIGEST_LENGTH];
+}
+
+- (SecKeyRef)publicKeyForKeyId:(NSString*)keyId {
+    NSData* appLabel = [TKUtil base64DecodeString:keyId];
+    CFMutableDictionaryRef getKeyRef = newCFDict;
+    
+    CFDictionarySetValue(getKeyRef, kSecClass, kSecClassKey);
+    CFDictionarySetValue(getKeyRef, kSecAttrKeyClass, kSecAttrKeyClassPublic);
+    CFDictionarySetValue(getKeyRef, kSecAttrApplicationLabel, (__bridge const void *)(appLabel));
+    CFDictionarySetValue(getKeyRef, kSecReturnRef, kCFBooleanTrue);
+    
+    SecKeyRef keyRef;
+    OSStatus status = SecItemCopyMatching(getKeyRef, (CFTypeRef *)&keyRef);
+    if (status != errSecSuccess) {
+        [NSException
+         raise:NSInvalidArgumentException
+         format:@"Error retrieving public key for id %@ error: %@\n", keyId,  @(status)];
+    }
+    
+    return (SecKeyRef)keyRef;
+}
 @end
---
+++ tests/TKSecureEnclaveTests.m
@@ -0,0 +1,68 @@
+//
+//  TKSecureEnclaveTests.m
+//  TokenSdk
+//
+//  Created by Vadim on 1/10/17.
+//  Copyright © 2017 Token Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "TKCrypto.h"
+#import "TKKeyInfo.h"
+#import "TKSecureEnclaveCryptoEngineFactory.h"
+#import "TKSignature.h"
+
+@interface TKSecureEnclaveTests : XCTestCase
+
+@end
+
+@implementation TKSecureEnclaveTests {
+    TKCrypto *crypto;
+}
+
+- (void)setUp {
+    [super setUp];
+    id<TKCryptoEngine> engine = [[[TKSecureEnclaveCryptoEngineFactory alloc] init] createEngine:@"member_id_123"];
+    crypto = [[TKCrypto alloc] initWithEngine:engine];
+}
+
+- (void)testSignAndVerify_message {
+    Token *token = [Token message];
+    token.payload.transfer.amount = @"100.23";
+    
+    TKKeyInfo *key = [crypto generateKey:Key_Level_Privileged];
+    TKSignature *signature = [crypto sign:token usingKey:kKeySigningHighPrivilege];
+    XCTAssertEqualObjects(signature.key.id, key.id);
+    XCTAssert(signature.value.length > 0);
+    
+    bool success = [crypto verifySignature:signature.value
+                                forMessage:token
+                                usingKeyId:key.id];
+    XCTAssert(success);
+}
+
+- (void)testSignAndVerify_token {
+    Token *token = [Token message];
+    token.payload.transfer.amount = @"100.23";
+    
+    TKKeyInfo *key = [crypto generateKey:Key_Level_Privileged];
+    TKSignature *signature = [crypto sign:token
+                                   action:TokenSignature_Action_Endorsed
+                                 usingKey:kKeySigningHighPrivilege];
+    XCTAssertEqualObjects(signature.key.id, key.id);
+    XCTAssert(signature.value.length > 0);
+    
+    bool success = [crypto verifySignature:signature.value
+                                  forToken:token
+                                    action:TokenSignature_Action_Endorsed
+                                usingKeyId:key.id];
+    XCTAssert(success);
+    
+    success = [crypto verifySignature:signature.value
+                             forToken:token
+                               action:TokenSignature_Action_Cancelled
+                           usingKeyId:key.id];
+    XCTAssert(!success);
+}
+
+@end
