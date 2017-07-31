308631629b98018d4f41d53a641d947172c0c85c
Sibin Lu <sibin.lu@token.io>
2017-07-31 16:19:38 -0700

    cover all onSuccess and onError invoker to non nullable

--- src/api/TKTypedef.h
+++ src/api/TKTypedef.h
@@ -24,50 +24,50 @@
 @class Pricing;
 @class Profile;
 
-typedef void (^OnSuccess)();
-typedef void (^OnError)( NSError * _Nonnull);
+typedef void (^ _Nonnull OnSuccess)();
+typedef void (^ _Nonnull OnError)( NSError * _Nonnull);
 
-typedef void (^OnSuccessWithBoolean)(BOOL);
-typedef void (^OnSuccessWithString)(NSString *_Nullable);
+typedef void (^ _Nonnull OnSuccessWithBoolean)(BOOL);
+typedef void (^ _Nonnull OnSuccessWithString)(NSString *_Nullable);
 
-typedef void (^OnSuccessWithMember)(Member * _Nonnull);
-typedef void (^OnSuccessWithTKMemberAsync)(TKMemberAsync * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithMember)(Member * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTKMemberAsync)(TKMemberAsync * _Nonnull);
 
-typedef void (^OnSuccessWithAccounts)(NSArray<Account *> * _Nonnull);
-typedef void (^OnSuccessWithTKAccountsAsync)(NSArray<TKAccountAsync *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithAccounts)(NSArray<Account *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTKAccountsAsync)(NSArray<TKAccountAsync *> * _Nonnull);
 
-typedef void (^OnSuccessWithAccount)(Account * _Nonnull);
-typedef void (^OnSuccessWithTKAccountAsync)(TKAccountAsync * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithAccount)(Account * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTKAccountAsync)(TKAccountAsync * _Nonnull);
 
-typedef void (^OnSuccessWithToken)(Token * _Nonnull);
-typedef void (^OnSuccessWithTokens)(PagedArray<Token *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithToken)(Token * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTokens)(PagedArray<Token *> * _Nonnull);
 
-typedef void (^OnSuccessWithTransfer)(Transfer * _Nonnull);
-typedef void (^OnSuccessWithTransfers)(PagedArray<Transfer *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTransfer)(Transfer * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTransfers)(PagedArray<Transfer *> * _Nonnull);
 
-typedef void (^OnSuccessWithBlob)(Blob *_Nullable);
+typedef void (^ _Nonnull OnSuccessWithBlob)(Blob *_Nullable);
 
-typedef void (^OnSuccessWithAttachment)(Attachment *_Nullable);
+typedef void (^ _Nonnull OnSuccessWithAttachment)(Attachment *_Nullable);
 
-typedef void (^OnSuccessWithMoney)(Money * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithMoney)(Money * _Nonnull);
 
-typedef void (^OnSuccessWithTransaction)(Transaction * _Nonnull);
-typedef void (^OnSuccessWithTransactions)(PagedArray<Transaction *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTransaction)(Transaction * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTransactions)(PagedArray<Transaction *> * _Nonnull);
 
-typedef void (^OnSuccessWithAddress)(AddressRecord * _Nonnull);
-typedef void (^OnSuccessWithAddresses)(NSArray<AddressRecord *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithAddress)(AddressRecord * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithAddresses)(NSArray<AddressRecord *> * _Nonnull);
 
-typedef void (^OnSuccessWithSubscriber)(Subscriber * _Nonnull);
-typedef void (^OnSuccessWithSubscribers)(NSArray<Subscriber *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithSubscriber)(Subscriber * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithSubscribers)(NSArray<Subscriber *> * _Nonnull);
 
-typedef void (^OnSuccessWithNotification)(Notification * _Nonnull);
-typedef void (^OnSuccessWithNotifications)(PagedArray<Notification *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithNotification)(Notification * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithNotifications)(PagedArray<Notification *> * _Nonnull);
 
-typedef void (^OnSuccessWithTokenOperationResult)(TokenOperationResult * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithTokenOperationResult)(TokenOperationResult * _Nonnull);
 
-typedef void (^OnSuccessWithBanks)(NSArray<Bank *> * _Nonnull);
-typedef void (^OnSuccessWithBankInfo)(BankInfo * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithBanks)(NSArray<Bank *> * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithBankInfo)(BankInfo * _Nonnull);
 
-typedef void (^OnSuccessWithDeviceInfo)(DeviceInfo * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithDeviceInfo)(DeviceInfo * _Nonnull);
 
-typedef void (^OnSuccessWithProfile)(Profile * _Nonnull);
+typedef void (^ _Nonnull OnSuccessWithProfile)(Profile * _Nonnull);
