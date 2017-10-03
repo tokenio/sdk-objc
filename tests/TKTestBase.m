223bf22b84cdf0955a4cb98fa640083be4be619d
Sibin Lu <sibin.lu@token.io>
2017-10-03 13:11:31 -0700

    all the alias value shall be lowercase

--- tests/TKTestBase.m
+++ tests/TKTestBase.m
@@ -184,14 +184,14 @@
 
 - (Alias *)generateEmailAlias {
     Alias *alias = [Alias new];
-    alias.value = [[@"alias-" stringByAppendingString:[TKUtil nonce]] stringByAppendingString:@"@token.io"];
+    alias.value = [[[@"alias-" stringByAppendingString:[TKUtil nonce]] stringByAppendingString:@"@token.io"] lowercaseString];
     alias.type = Alias_Type_Email;
     return alias;
 }
 
 - (Alias *)generatePhoneAlias {
     Alias *alias = [Alias new];
-    alias.value = [[@"alias-" stringByAppendingString:[TKUtil nonce]] stringByAppendingString:@"@token.io"];
+    alias.value = [[[@"alias-" stringByAppendingString:[TKUtil nonce]] stringByAppendingString:@"@token.io"] lowercaseString];
     alias.type = Alias_Type_Phone;
     return alias;
 }
