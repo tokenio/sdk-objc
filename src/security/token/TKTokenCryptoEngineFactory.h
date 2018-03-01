47f6d1357a932a8229270b88779a30d9569375bb
Sibin Lu <sibin.lu@token.io>
2018-02-28 16:47:29 -0800

    fix comment

--- src/security/token/TKTokenCryptoEngineFactory.h
+++ src/security/token/TKTokenCryptoEngineFactory.h
@@ -16,7 +16,7 @@
  * @param storage the customized key store
  * @param useLocalAuthentication require local authentication to sign data. If you are using your
  * own key storage, Token crypto engine will ask for local authentication when you sign data
- * by default. Disables this will skip the local authentication
+ * by default. Disabling this will skip the local authentication
  */
 + (id<TKCryptoEngineFactory>)factoryWithStore:(id<TKKeyStore>)storage
                        useLocalAuthentication:(BOOL)useLocalAuthentication;
