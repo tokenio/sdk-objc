2c7cd11dad2e05b30ea816850aab29f6bfe3ffec
Sibin Lu <sibin.lu@token.io>
2018-04-09 13:39:14 -0700

    fix nit

--- src/util/TKUtil.m
+++ src/util/TKUtil.m
@@ -133,12 +133,15 @@
     return output;
 }
 
-+ (NSString *)randomStringWithLength: (int) len {
-    static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
++ (NSString *)randomStringWithLength:(int)len {
+    static NSString * const letters =
+    @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
     NSMutableString *randomString = [NSMutableString stringWithCapacity: len];
     
     for (int i=0; i<len; i++) {
-        [randomString appendFormat: @"%C", [letters characterAtIndex: arc4random_uniform((uint32_t)[letters length])]];
+        [randomString appendFormat: @"%C",
+         [letters characterAtIndex: arc4random_uniform((uint32_t)[letters length])]];
     }
     
     return randomString;
