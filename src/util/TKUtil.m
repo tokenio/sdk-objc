31859172bda3526a47118d4724cbfaaf55e04079
Sibin Lu <sibin.lu@token.io>
2018-04-09 12:25:57 -0700

    change nonce to random string(20)

--- src/util/TKUtil.m
+++ src/util/TKUtil.m
@@ -15,7 +15,7 @@
 }
 
 + (NSString *)nonce {
-    return [NSUUID UUID].UUIDString;
+    return [self randomStringWithLength:20];
 }
 
 + (NSString *)base64EncodeBytes:(const char *)bytes length:(NSUInteger)length {
@@ -130,6 +130,18 @@
             [output appendFormat:@"%C", c];
         }
     }
-    return output;}
+    return output;
+}
+
++ (NSString *)randomStringWithLength: (int) len {
+    static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    NSMutableString *randomString = [NSMutableString stringWithCapacity: len];
+    
+    for (int i=0; i<len; i++) {
+        [randomString appendFormat: @"%C", [letters characterAtIndex: arc4random_uniform((uint32_t)[letters length])]];
+    }
+    
+    return randomString;
+}
 
 @end
